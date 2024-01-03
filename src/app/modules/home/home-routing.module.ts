26a9c2f32720ed429120b97ebafbf7386fb6268c
jizquierdo <javieraizquierdo@MacBook-Pro-de-Javiera.local>
2024-01-03 16:14:43 -0300

    add routes

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ":", //TODO: localhost:4200
+    component: HomePageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
