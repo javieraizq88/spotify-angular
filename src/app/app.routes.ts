3fca6390800224df58722ee8bad1fb7d31bd5f14
jizquierdo <javieraizquierdo@MacBook-Pro-de-Javiera.local>
2024-01-03 13:58:27 -0300

    add component basic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+      path: "", //lTODO: ocalhost4200/
+      component:ExampleComponent
+    },
+    {
+      path: "hello", //lTODO: ocalhost4200/
+      component:ExampleComponent
+    }
+
+];
