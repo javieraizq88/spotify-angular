7d5a5072ae94510f6c46ad1cfa1bb9f9008e02de
jizquierdo <javieraizquierdo@MacBook-Pro-de-Javiera.local>
2024-01-04 12:30:51 -0300

    add shared component dinamyc

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,24 +1,78 @@
-import { NgFor } from '@angular/common';
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
   imports: [
-    NgFor
+    NgFor,
+    NgClass
   ],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.less'
 })
 export class SideBarComponent {
-  linksMenu: Array<any> = [
-    {
-      name: "home",
-      icon: "uil-estate"
-    },
-    {
-      name: "buscar",
-      icon: "uil-estate"
-    },
-  ]
+
+  mainMenu: {
+    defaultOptions: Array<any>, accessLink: Array<any>
+  } = { defaultOptions: [], accessLink: [] };
+
+  customOptions:  Array<any> = []
+
+  constructor () { }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil uil-estate',
+        router: ['/', 'auth']
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil uil-search',
+        router: ['/', 'history']
+      },
+      {
+        name: 'Tu biblioteca',
+        icon: 'uil uil-chart',
+        router: ['/', 'favorites'],
+        query: { hola: 'mundo' }
+      }
+    ]
+
+
+    this.mainMenu.accessLink = [
+      {
+        name: 'Crear lista',
+        icon: 'uil-plus-square'
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil-heart-medical'
+      }
+    ]
+
+    this.customOptions = [
+      {
+        name: 'Mi lista º1',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º2',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º3',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º4',
+        router: ['/']
+      }
+    ]
+
+  }
 }
