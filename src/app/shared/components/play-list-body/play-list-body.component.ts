c877de8affa6960aec8b00577835036502d9d2c5
jizquierdo <javieraizquierdo@MacBook-Pro-de-Javiera.local>
2024-01-07 14:01:07 -0300

    se crea en component de play list body el const para traer la data y se importa ngcomponent y ngfor

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as dataRaw from "../../../data/tracks.json"
+import { TrackModel } from '@core/models/tracks.model';
+import { NgFor, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-play-list-body',
   standalone: true,
-  imports: [],
+  imports: [
+    NgTemplateOutlet,
+    NgFor
+  ],
   templateUrl: './play-list-body.component.html',
   styleUrl: './play-list-body.component.less'
 })
-export class PlayListBodyComponent {
+export class PlayListBodyComponent  implements OnInit {
+ tracks: TrackModel [] = []
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const { data }: any = (dataRaw as any).default
+    this.tracks = data;
+    console.log(data)
+  }
 
 }
