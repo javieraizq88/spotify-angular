0168e496e3f805df2610ab82d466846ac27ad1b0
jizquierdo <javieraizquierdo@MacBook-Pro-de-Javiera.local>
2024-01-08 20:53:48 -0300

    form group llamando a propiedades en input

--- src/app/modules/history/pages/history-page/history-page.component.ts
+++ src/app/modules/history/pages/history-page/history-page.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, TRANSLATIONS } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-history-page',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule,  FormsModule, NgIf],
   templateUrl: './history-page.component.html',
   styleUrl: './history-page.component.less'
 })
-export class HistoryPageComponent {
+export class HistoryPageComponent implements OnInit {
 
+  // cada input tiene su propio estado con valor y validacion
+  formLogin: FormGroup = new FormGroup({});
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.formLogin = new FormGroup(
+      {
+        email: new FormControl(""),
+        password: new FormControl(""),
+      }
+    )
+
+  }
 }
