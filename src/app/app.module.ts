aabe2596e80a791ac35aff92eaf58b4d058a3223
jizquierdo <javieraizquierdo@MacBook-Pro-de-Javiera.local>
2024-01-08 11:01:25 -0300

    add pipe

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { Component, Injectable, NgModule, Pipe } from '@angular/core';
+import { CommonModule, NgClass, NgFor, NgForOf } from '@angular/common';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HomeModule } from './modules/home/home.module';
+import { SharedModule } from '@shared/shared.module';
+import { TracksModule } from '@modules/tracks/tracks.module';
+import { HistoryModule } from '@modules/history/history.module';
+import { AuthModule } from '@modules/auth/auth.module';
+import { CardPlayerComponent } from '@shared/components/card-player/card-player.component';
+import { AppComponent } from './app.component';
+
+
+
+@NgModule({
+  declarations:   [      ],
+  imports: [
+        RouterModule,
+    AppComponent,
+    Injectable,
+
+  ],
+  exports: [
+    RouterModule,
+    AppComponent,
+
+  ],
+  providers:    [  ],
+})
+export class AppModule { }
