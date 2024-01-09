3a60fc202dd3973ec5d97bce322a5b3c6d860a2b
jizquierdo <javieraizquierdo@MacBook-Pro-de-Javiera.local>
2024-01-09 14:43:37 -0300

    tracks funcionando sin errores

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,15 +1,13 @@
-import { NgClass, NgFor } from '@angular/common';
 import { Component, Directive, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '@shared/shared.module';
+import * as dataRaw from "../../../../data/tracks.json";
 
 import { TrackService } from '../../services/track.service';
 import { response } from 'express';
 import { Subscription } from 'rxjs';
 import { TracksModule } from '../../tracks.module';
 import { timingSafeEqual } from 'crypto';
-
-
 @Component({
   selector: 'app-tracks-page',
   standalone: true,
@@ -20,38 +18,39 @@ import { timingSafeEqual } from 'crypto';
   templateUrl: './tracks-page.component.html',
   styleUrl: './tracks-page.component.less'
 })
+
 export class TracksPageComponent implements OnInit {
 
   cancionesTrending: Array<TracksModule> = []
   cancionesRandom: Array<TracksModule> = []
-
   listObservers$: Array<Subscription> = []
 
   constructor(private trackService: TrackService) { }
 
   ngOnInit(): void {
-    // muestra un array estático con las canciones
-    // const { data }: any = (dataRaw as any).default
-    // this.mockTrackList = data;
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.listObservers$.forEach(u => u.unsubscribe())
 
-    // const observer1$ = this.trackService.dataCancionesTrending$
-    //   .subscribe(response => {
-    //     this.cancionesTrending = response
-    //     this.cancionesRandom = response
-    //     console.log("canciones trending ", response);
-    //   })
+    const observer1$ = this.trackService.dataCancionesTrending$
+      .subscribe(response => {
+        this.cancionesTrending = response
+        this.cancionesRandom = response
+        console.log("canciones trending ", response);
+      })
 
-    // const observer2$ = this.trackService.dataCancionesRandom$
-    //   .subscribe(response => {
-    //     // concatena la cancion random al arreglo de canciones trending y lo muestra a los 4 seg en la seccion de abajo
-    //     this.cancionesRandom = [...this.cancionesRandom, ...response]
-    //     console.log("canciones random entrando ", response);
-    //   })
-    // this.listObservers$ = [observer1$, observer2$]
+    const observer2$ = this.trackService.dataCancionesRandom$
+      .subscribe(response => {
+        // concatena la cancion random al arreglo de canciones trending y lo muestra a los 4 seg en la seccion de abajo
+        this.cancionesRandom = [...this.cancionesRandom, ...response]
+        console.log("canciones random entrando ", response);
+      })
+    this.listObservers$ = [observer1$, observer2$]
   }
 
   ngOnDestroy(): void {
-    // this.listObservers$.forEach(u => u.unsubscribe())
-
+//Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.listObservers$.forEach(u => u.unsubscribe())
   }
 }
--- src/app/modules/tracks/services/track.service.ts
+++ src/app/modules/tracks/services/track.service.ts
@@ -1,39 +1,32 @@
 import { Injectable } from '@angular/core';
 import { observable, Observable, of } from 'rxjs';
 import { TracksModule } from '../tracks.module';
+import * as dataRaw from "../../../data/tracks.json"
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class TrackService {
 
   dataCancionesTrending$: Observable<TracksModule[]> = of ([])
   dataCancionesRandom$: Observable<any> = of ([])
 
-  constructor(private httpClient: HttpClient) {
-    // const { data }: any = (dataRaw as any).default;
-
-    // this.dataCancionesTrending$ = of(data)
-
-    // this.dataCancionesRandom$ = new Observable((observable) => {
-
-    //   const trackExample: TracksModule = {
-    //     _id: 9,
-    //     name: "asdasdasd",
-    //     album: "album",
-    //     url: "http://",
-    //     cover: "https://picsum.photos/200/300?grayscale"
-    //   }
-
-    //   setTimeout(() => {
-    //     observable.next([trackExample])
-    //   },3500)
-    // })
+  constructor() {
+    const { data }: any = (dataRaw as any).default;
+    this.dataCancionesTrending$ = of(data)
+    this.dataCancionesRandom$ = new Observable((observable) => {
+      const trackExample: TracksModule = {
+        _id: 9,
+        name: "asdasdasd",
+        album: "album",
+        url: "http://",
+        cover: "https://picsum.photos/200/300?grayscale"
+      }
+      setTimeout(() => {
+        observable.next([trackExample])
+      },3500)
+    })
   }
 
-  getAllTracks$(): Observable {
-    
-  }
 }
