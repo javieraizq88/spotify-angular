a51c6b9e6a8376fc117acb730dcb497f73b56f86
jizquierdo <javieraizquierdo@MacBook-Pro-de-Javiera.local>
2024-01-16 10:32:09 -0300

    play list body spect arreglado

--- src/app/shared/components/play-list-body/play-list-body.component.spec.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlayListBodyComponent } from './play-list-body.component';
+import { OrderListPipe } from '../../pipe/order-list.pipe';
 
 describe('PlayListBodyComponent', () => {
   let component: PlayListBodyComponent;
@@ -8,10 +9,13 @@ describe('PlayListBodyComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PlayListBodyComponent]
+      imports: [
+        PlayListBodyComponent,
+        OrderListPipe
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(PlayListBodyComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
