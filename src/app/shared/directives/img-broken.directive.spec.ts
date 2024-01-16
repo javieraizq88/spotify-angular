54008ceb93aaa090717be920bf84be151936ab7d
jizquierdo <javieraizquierdo@MacBook-Pro-de-Javiera.local>
2024-01-16 09:56:23 -0300

    solucion error typescript

--- src/app/shared/directives/img-broken.directive.spec.ts
+++ src/app/shared/directives/img-broken.directive.spec.ts
@@ -1,9 +1,14 @@
 import { ElementRef } from '@angular/core';
 import { ImgBrokenDirective } from './img-broken.directive';
 
+
+// TODO: la prueba de img broken es la siguiente
 describe('ImgBrokenDirective', () => {
+
+  //TODO: deberia instanciar correctamente
   it('should create an instance', () => {
-    const directive = new ImgBrokenDirective();
+    const mockElement = new ElementRef("")
+    const directive = new ImgBrokenDirective(mockElement);
     expect(directive).toBeTruthy();
   });
 });
