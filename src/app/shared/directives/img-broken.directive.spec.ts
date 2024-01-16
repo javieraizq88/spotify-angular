9b7df7a96fb106440d335452257de697e824b085
jizquierdo <javieraizquierdo@MacBook-Pro-de-Javiera.local>
2024-01-16 17:36:56 -0300

    test directive

--- src/app/shared/directives/img-broken.directive.spec.ts
+++ src/app/shared/directives/img-broken.directive.spec.ts
@@ -1,9 +1,36 @@
-import { ElementRef } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { ImgBrokenDirective } from './img-broken.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { after } from 'node:test';
 
+// componente de prueba
+
+@Component({
+  template: '<img class="testing-directive" appImgBroken [src]="srcMock">'
+})
+class TestComponent {
+  public srcMock: any = null
+}
 
 // TODO: la prueba de img broken es la siguiente
 describe('ImgBrokenDirective', () => {
+
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        TestComponent,
+        ImgBrokenDirective
+      ]
+    })
+
+    fixture = TestBed.createComponent(TestComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges() // detecta los cambios en la directiva
+  })
 
   //TODO: deberia instanciar correctamente
   it('should create an instance', () => {
@@ -11,4 +38,29 @@ describe('ImgBrokenDirective', () => {
     const directive = new ImgBrokenDirective(mockElement);
     expect(directive).toBeTruthy();
   });
-});
+
+  //TODO: deberia instanciar correctamente
+  it('testComponent deberia instanciarse correctamente', () => {
+    expect(component).toBeTruthy();
+  });
+
+  //TODO: deberia instanciar correctamente
+  it('directiva deberia cambiar la imagen por un base 64', (done: DoneFn) => {
+    // Arrange
+    const beforeImgElement = fixture.debugElement.query(By.css('testing-directive')).nativeElement
+    const beforeImgSrc = beforeImgElement.src // url antes de ser cambiada por la directiva
+    component.srcMock = undefined
+
+    setTimeout(() => {
+      const afterImgElement = fixture.debugElement.query(By.css('testing-directive')).nativeElement
+      const afterImgSrc = afterImgElement.src // url despues de ser cambiada por la directiva
+
+      // expect(afterImgElement).toEqual("https://fastly.picsum.photos/id/573/200/300.jpg?blur=2&hmac=r50TUpMK1Y2-iE0tZO0Zr8QCdPzXz64-9JfdM2fNTeA")
+      expect(afterImgElement).toMatch(/\bdata:image\b/) // cuando una cadena de texto que tenga bdata:image ---> prueba exitosa
+      done() // fin del test
+    }, 3000);
+
+    
+  });
+
+})
--- src/app/shared/directives/img-broken.directive.ts
+++ src/app/shared/directives/img-broken.directive.ts
@@ -4,11 +4,23 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
   selector: 'img[appImgBroken]'
 })
 export class ImgBrokenDirective {
-  @Input() customImg: string = ''
+  @Input() customImg: string | boolean = false
   @HostListener('error') handleError(): void {
     const elNative = this.elHost.nativeElement
     console.log('🔴 Esta imagen revento -->', this.elHost);
-    elNative.src = this.customImg
+
+    if (this.customImg) {
+      elNative.src = this.customImg
+    } else {
+      // opcion 1 ---> agregar img en el proyecto
+      // elNative.src = "../.."
+
+      //opcion 2 ---> agregar una img desde una pag web
+      // elNative.src = "https://fastly.picsum.photos/id/573/200/300.jpg?blur=2&hmac=r50TUpMK1Y2-iE0tZO0Zr8QCdPzXz64-9JfdM2fNTeA"
+
+      // opcion 3 ---> agregar una img base 64
+      elNative.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAAXNSR0IArs4c6QAAAD5JREFUGFdtjKENAEAIA4vC49h/HWbAERwexYfXVDW9S8nMRkTAzOhuVBXI3WeLqiIzsQJFxAD4w4LNbV6fD32XLKyP2o8zAAAAAElFTkSuQmCC"
+    }
 
   }
 
